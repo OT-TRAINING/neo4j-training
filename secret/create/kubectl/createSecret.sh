2a21520a35d6a597ccec28e2b46514be9b040a3a
SANDEEP RAWAT <sandeep@opstree.com>
2022-04-03 15:26:21 +0530

    Added function to secrets creation

--- secret/create/kubectl/createSecret.sh
+++ secret/create/kubectl/createSecret.sh
@@ -2,7 +2,7 @@
 
 function _logMessage() {
     local message=$1
-
+    echo ""
     echo "${message}"
 }
 
@@ -18,14 +18,28 @@ function _runCommand() {
 }
 
 function _clearScreen() {
+    echo ""
     read -p "Press a key to clear screen......" ignore
     clear
 }
 
 function showCaseSecretCreationFromFile() {
-    _runCommand "kubectl create secret generic db-creds --from-file=./DB_CREDS.properties"
+    _runCommand "kubectl create secret generic db-creds --from-file=./USERNAME --from-file=./PASSWORD"
     _runCommand "kubectl get secrets"
     _runCommand "kubectl describe secret db-creds"
+    _runCommand "kubectl get secret db-creds -o jsonpath='{.data.USERNAME}' | base64 --decode"
+    _runCommand "kubectl get secret db-creds -o jsonpath='{.data.PASSWORD}' | base64 --decode"
+    _clearScreen
+
+    _runCommand "kubectl delete secret db-creds"
+}
+
+function showCaseSecretCreationFromLiteral() {
+    _runCommand "kubectl create secret generic db-creds --from-literal=USERNAME=opstree --from-literal=PASSWORD=password"
+    _runCommand "kubectl get secrets"
+    _runCommand "kubectl describe secret db-creds"
+    _runCommand "kubectl get secret db-creds -o jsonpath='{.data.USERNAME}' | base64 --decode"
+    _runCommand "kubectl get secret db-creds -o jsonpath='{.data.PASSWORD}' | base64 --decode"
     _clearScreen
 
     _runCommand "kubectl delete secret db-creds"
