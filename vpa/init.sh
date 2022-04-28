2c8e4f42f101d78299d3f2af2000cca5dc4f43bc
bhupender rawat <bhupi212@fmail.com>
2022-04-28 17:14:50 +0530

    added content for vpa

---
+++ vpa/init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VPA_GITHUB_LINK="https://github.com/kubernetes/autoscaler.git"
+CA_LOCATION="/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+TOKEN=$(cat /run/secrets/kubernetes.io/serviceaccount/token)
+
+if [[ "${VPA_GITHUB_LINK_BRANCH}" != "master" ]];
+then
+    if [[ "${VPA_GITHUB_LINK_BRANCH}" != "" ]];
+    then
+    rm -rf /opt/vpa/autoscaler
+    git clone -b ${VPA_GITHUB_LINK_BRANCH} ${VPA_GITHUB_LINK}
+    fi
+fi
+
+KUBECTL_VERSION=$(curl -s --cacert $CA_LOCATION -X GET https://${API_SERVER}:8443/version --header "Authorization: Bearer ${TOKEN}"  | jq '.gitVersion' | awk -F\" '{print $2}')
+
+wget "https://dl.k8s.io/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl"
+
+mv /opt/vpa/kubectl /bin
+
+chmod +x /bin/kubectl
+
+/bin/bash /opt/vpa/autoscaler/vertical-pod-autoscaler/hack/vpa-up.sh
